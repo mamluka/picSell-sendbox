b2af6b861374f84c96c384f23524e393a26d96a2
david <david.mazvovsky@gmail.com>
2013-12-21 21:48:40 +0200

    update script

--- mine-by-category.sh
+++ mine-by-category.sh
@@ -1,9 +1,10 @@
 category=$1
 
 ./ebay-mining-tool.rb mine_by_category $category 5
-./ebay-mining-tool.rb generate-matches ebay-phones.json $category > pre-matched-$category.json
-./amazon-mining-tool.rb match pre-matched-phones.json phones > matched-$category.json
+./ebay-mining-tool.rb generate-matches ebay-$category.json $category > pre-matched-$category.json
+./amazon-mining-tool.rb match pre-matched-$category.json $category > matched-$category.json
 ./amazon-mining-tool.rb extract-asins matched-$category.json > asins/$category
+cat asins/$category | ./amazon-mining-tool.rb mine $category --use-asins --allow-no-categories --allow-any-sales-ranking
 ./pipe-tool.rb combine ebay-$category.json amazon-$category.json matched-$category.json > combined-$category.json
 ./pipe-tool.rb mash combined-$category.json $category > indexable-$category.json
 ./product-indexing-tool.rb index indexable-$category.json
